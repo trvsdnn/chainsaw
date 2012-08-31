6f666918554547166784bcfe49839ae12751132e
blahed <tdunn13@gmail.com>
2012-08-31 01:37:54 -0400

    add pattern test

--- test/detector_test.rb
+++ test/detector_test.rb
@@ -3,6 +3,15 @@ require 'test_helper'
 describe Chainsaw::Detector do
   Detector = Chainsaw::Detector
 
+  it 'detects a log format and returns a pattern given a Time' do
+    line         = get_log_line('apache_access.log')
+    detected     = Detector.detect(line)
+    now          = Time.now
+    time_pattern = now.strftime(detected.time_format)
+
+    detected.pattern(now).must_equal /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - - \[#{time_pattern} -\d{4}\]/i
+  end
+
   it 'detects an apache access log format' do
     line     = get_log_line('apache_access.log')
     detected = Detector.detect(line)
