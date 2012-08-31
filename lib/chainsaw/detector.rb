957ca7427592f646d1ab210cc4a73d1da6d4b2bc
ljfauscett <ljfauscett@gmail.com>
2012-08-31 01:17:39 -0400

    added more log regex and tests

--- lib/chainsaw/detector.rb
+++ lib/chainsaw/detector.rb
@@ -1,6 +1,8 @@
 module Chainsaw
   class Detector
     APACHE_ACCESS_REGEX = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - - \[\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} -\d{4}\]/i
+    APACHE_ERROR_REGEX  = /^\[[a-z]{3} [a-z]{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}\]/i
+    NGINX_ERROR_LOG     = /\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}/
 
     attr_accessor :type
     attr_accessor :time_format
@@ -15,6 +17,12 @@ module Chainsaw
       when APACHE_ACCESS_REGEX
         detected.type        = 'apache_access'
         detected.time_format = '%d/%b/%Y:%H:%M:%S'
+      when APACHE_ERROR_REGEX
+        detected.type        = 'apache_error'
+        detected.time_format = '%a /%b /%d %H:%M:%S %Y'
+      when NGINX_ERROR_LOG
+        detected.type = 'nginx_error'
+        detected.time_format = '%Y/%m/%d %H:%M:%S'
       end
 
       detected
--- test/detector_test.rb
+++ test/detector_test.rb
@@ -11,6 +11,22 @@ describe Chainsaw::Detector do
     detected.time_format.must_equal '%d/%b/%Y:%H:%M:%S'
   end
 
+  it 'detects an apache error log format' do
+    line     = get_log_line('apache_error.log')
+    detected = Detector.detect(line)
+
+    detected.type.must_equal 'apache_error'
+    detected.time_format.must_equal '%a /%b /%d %H:%M:%S %Y'
+  end
+
+  it 'detects an nginx error log format' do
+    line     = get_log_line('nginx_error.log')
+    detected = Detector.detect(line)
+
+    detected.type.must_equal 'nginx_error'
+    detected.time_format.must_equal '%Y/%m/%d %H:%M:%S'
+  end
+
   def get_log_line(logfile)
     logfile = File.expand_path("../logs/#{logfile}", __FILE__)
     File.open(logfile).first
