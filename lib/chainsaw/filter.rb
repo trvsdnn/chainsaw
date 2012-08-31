52195dcfd4558828c20fb437bf9a8c20d3ec7ee6
blahed <tdunn13@gmail.com>
2012-08-31 13:57:57 -0400

    refactor filter

--- lib/chainsaw/filter.rb
+++ lib/chainsaw/filter.rb
@@ -1,33 +1,48 @@
 module Chainsaw
   class Filter
-    def self.filter(logfile, interval, options)
-      log        = File.open(logfile)
-      detected   = Detector.detect(log.first)
-      end_at     = interval
-      line_count = 0
+    def initialize(logfile, interval, options)
+      @logfile  = logfile
+      @interval = interval
+      @options  = options
 
-      log.rewind
+      @log = File.open(@logfile)
+      # TODO: rename detected
+      @detected = Detector.detect(@log.first)
+      @log.rewind
 
-      log.each_line do |line|
-        timestamp = line.match(detected.pattern)[1]
-        time      = DateTime.strptime(timestamp, detected.time_format).to_time
+      self
+    end
 
-        if end_at < time
-          line_count += 1
+    def start
+      @line_count = 0
+      end_at     = @interval
 
-          if options.output_file
-            File.open(options.output_file, 'a') { |f| f.write(line) }
-          elsif options.colorize
-            puts line.sub(timestamp, "\033[32m#{timestamp}\033[0m")
-          else
-            puts line
-          end
+      @log.each_line do |line|
+        timestamp = line.match(@detected.pattern)[1]
+        time      = DateTime.strptime(timestamp, @detected.time_format).to_time
 
-          STDIN.gets if options.interactive && !options.output_file
-        end
+        found(line, timestamp) if end_at < time
       end
 
-      puts "\nFound #{line_count} line(s)" unless options.output_file
+      puts "\nFound #{@line_count} line(s)" unless @options.output_file
+    end
+
+    def found(line, timestamp)
+      @line_count += 1
+
+      if @options.output_file
+        File.open(@options.output_file, 'a') { |f| f.write(line) }
+      elsif @options.colorize
+        puts line.sub(timestamp, "\033[32m#{timestamp}\033[0m")
+      else
+        puts line
+      end
+
+      STDIN.gets if @options.interactive && !@options.output_file
+    end
+
+    def self.filter(*args)
+      new(*args).start
     end
   end
 end
