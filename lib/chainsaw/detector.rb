7fd053b0a89ab5cb478f71238ac2616e9885a659
blahed <tdunn13@gmail.com>
2012-08-31 01:37:41 -0400

    fixing conflicts

--- lib/chainsaw/detector.rb
+++ lib/chainsaw/detector.rb
@@ -1,26 +1,53 @@
 module Chainsaw
   class Detector
-    APACHE_ACCESS_REGEX = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - - \[\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} -\d{4}\]/i
-    APACHE_ERROR_REGEX  = /^\[[a-z]{3} [a-z]{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}\]/i
-    NGINX_ERROR_LOG     = /\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}/
-
     attr_accessor :type
     attr_accessor :time_format
 
     def initialize
     end
 
+    def pattern(time = Time.now)
+      self.class.send("#{type}_pattern".to_sym, time.strftime(time_format))
+    end
+
+    # TODO: rename this stupid shit
+    def self.time_pattern_as_regexp(time_pattern)
+      if time_pattern.is_a? Regexp
+        time_pattern = time_pattern.source
+      else
+        time_pattern = Regexp.escape(time_pattern)
+      end
+    end
+
+    def self.apache_access_pattern(time_pattern = /\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2}/)
+      time_pattern = time_pattern_as_regexp(time_pattern)
+
+      /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - - \[#{time_pattern} -\d{4}\]/i
+    end
+
+    def self.apache_error_pattern(time_pattern = /^\[[a-z]{3} [a-z]{3} \d{2} \d{2}:\d{2}:\d{2} \d{4}\]/)
+      time_pattern = time_pattern_as_regexp(time_pattern)
+
+      /^#{time_pattern} \[[a-z]+\]/i
+    end
+
+    def self.nginx_error_pattern(time_pattern = /\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}:\d{2}/)
+      time_pattern = time_pattern_as_regexp(time_pattern)
+
+      /^#{time_pattern} \[[a-z]+\]/i
+    end
+
     def self.detect(line)
       detected = new
 
       case line
-      when APACHE_ACCESS_REGEX
+      when apache_access_pattern
         detected.type        = 'apache_access'
         detected.time_format = '%d/%b/%Y:%H:%M:%S'
-      when APACHE_ERROR_REGEX
+      when apache_error_pattern
         detected.type        = 'apache_error'
         detected.time_format = '%a /%b /%d %H:%M:%S %Y'
-      when NGINX_ERROR_LOG
+      when nginx_error_pattern
         detected.type = 'nginx_error'
         detected.time_format = '%Y/%m/%d %H:%M:%S'
       end
