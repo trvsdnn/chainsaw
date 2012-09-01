2789ad64b1a05acf0b8d3e00d090b0aae88f7d9d
blahed <tdunn13@gmail.com>
2012-09-01 11:53:15 -0400

    clean up tests, add ruby_logger format

--- lib/chainsaw/detector.rb
+++ lib/chainsaw/detector.rb
@@ -3,7 +3,8 @@ module Chainsaw
 
     CLF_PATTERN          = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} (?:-|[^ ]+) (?:-|[^ ]+) \[(\d{2}\/[a-z]{3}\/\d{4}:\d{2}:\d{2}:\d{2} -\d{4})\]/i
     APACHE_ERROR_PATTERN = /^\[[a-z]{3} [a-z]{3} \d{2} (\d{2}:\d{2}:\d{2}) \d{4}\]/i
-    NGINX_ERROR_PATTERN  = /\d{4}\/\d{2}\/\d{2} (\d{2}:\d{2}:\d{2})/
+    NGINX_ERROR_PATTERN  = /\d{4}\/\d{2}\/\d{2} (\d{2}:\d{2}:\d{2})/i
+    RUBY_LOGGER_PATTERN  = /[a-z]{1}, \[(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})\.\d+ #\d+\]/i
 
     def self.detect(line)
       format = Format.new
@@ -21,6 +22,10 @@ module Chainsaw
         format.type        = 'nginx_error'
         format.time_format = '%Y/%m/%d %H:%M:%S'
         format.pattern     = NGINX_ERROR_PATTERN
+      when RUBY_LOGGER_PATTERN
+        format.type        = 'ruby_logger'
+        format.time_format = '%Y-%m-%dT%H:%M:%S'
+        format.pattern     = RUBY_LOGGER_PATTERN
       end
 
       format
--- test/detector_test.rb
+++ test/detector_test.rb
@@ -5,26 +5,34 @@ describe Chainsaw::Detector do
 
   it 'detects CLF log format' do
     line     = get_log_line('clf.log')
-    detected = Detector.detect(line)
+    format = Detector.detect(line)
 
-    detected.type.must_equal 'clf'
-    detected.time_format.must_equal '%d/%b/%Y:%H:%M:%S %z'
+    format.type.must_equal 'clf'
+    format.time_format.must_equal '%d/%b/%Y:%H:%M:%S %z'
   end
 
   it 'detects an apache error log format' do
     line     = get_log_line('apache_error.log')
-    detected = Detector.detect(line)
+    format = Detector.detect(line)
 
-    detected.type.must_equal 'apache_error'
-    detected.time_format.must_equal '%a /%b /%d %H:%M:%S %Y'
+    format.type.must_equal 'apache_error'
+    format.time_format.must_equal '%a /%b /%d %H:%M:%S %Y'
   end
 
   it 'detects an nginx error log format' do
     line     = get_log_line('nginx_error.log')
-    detected = Detector.detect(line)
+    format = Detector.detect(line)
 
-    detected.type.must_equal 'nginx_error'
-    detected.time_format.must_equal '%Y/%m/%d %H:%M:%S'
+    format.type.must_equal 'nginx_error'
+    format.time_format.must_equal '%Y/%m/%d %H:%M:%S'
+  end
+
+  it 'detects an ruby logger log format' do
+    line     = get_log_line('ruby_logger.log')
+    format = Detector.detect(line)
+
+    format.type.must_equal 'ruby_logger'
+    format.time_format.must_equal '%Y-%m-%dT%H:%M:%S'
   end
 
   def get_log_line(logfile)
