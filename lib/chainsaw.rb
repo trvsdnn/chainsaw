e031bd1ed8bae866b62d734b31cbcf16299c2436
blahed <tdunn13@gmail.com>
2012-10-04 02:23:50 -0400

    only set incoding in ruby 1.9*

--- lib/chainsaw.rb
+++ lib/chainsaw.rb
@@ -11,8 +11,10 @@ require 'chainsaw/detector'
 require 'chainsaw/filter'
 require 'chainsaw/version'
 
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+if defined? Encoding
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
 
 module Chainsaw
 
